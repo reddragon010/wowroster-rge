d1edaaccf9fd172fadd739c6b0761337992e491b
reddragon010 <reddragon010@gmx.net>
2009-04-16 21:24:07 +0200

    inital fork of the roster-202-with-imagepack

---
+++ lib/dbal/structure/upgrade_200.sql
@@ -0,0 +1,7 @@
+#
+# MySQL WoWRoster Upgrade File
+#
+# * $Id: upgrade_200.sql 1905 2008-11-13 05:53:27Z Zanix $
+#
+# --------------------------------------------------------
+### No DB Changes
---
+++ lib/dbal/structure/upgrade_201.sql
@@ -0,0 +1,7 @@
+#
+# MySQL WoWRoster Upgrade File
+#
+# * $Id: upgrade_201.sql 1920 2008-11-23 23:55:21Z Zanix $
+#
+# --------------------------------------------------------
+### No DB Changes
